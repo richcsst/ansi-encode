951b8985fb9870285c9c58c99b977c155f10b3c3
Richard Kelsch <rich@rk-internet.com>
2025-10-06 02:16:28 -0600

    Fixed test

--- lib/Term/ANSIEncode.pm
+++ lib/Term/ANSIEncode.pm
@@ -32,9 +32,9 @@ use utf8;
 use charnames();
 use constant {
     TRUE  => 1,
-	  FALSE => 0,
-	  YES   => 1,
-	  NO    => 0,
+    FALSE => 0,
+    YES   => 1,
+    NO    => 0,
 };
 
 use Term::ANSIScreen qw( :cursor :screen );
@@ -48,92 +48,92 @@ binmode(STDOUT, ":encoding(UTF-8)");
 binmode(STDIN,  ":encoding(UTF-8)");
 
 BEGIN {
-    our $VERSION = '1.30';
+    our $VERSION = '1.31';
 }
 
 sub ansi_decode {
-	my $self = shift;
-	my $text = shift;
+    my $self = shift;
+    my $text = shift;
 
-	if (length($text) > 1) {
-		while ($text =~ /\[\%\s+LOCATE (\d+),(\d+)\s+\%\]/) {
-			my ($r,$c) = ($1,$2);
-			my $replace = $self->{'ansi_sequences'}->{'CSI'} . "$r;$c" . 'H';
-			$text =~ s/\[\%\s+LOCATE $r,$c\s+\%\]/$replace/g;
-		}
-		while ($text =~ /\[\%\s+SCROLL UP (\d+)\s+\%\]/) {
-			my $s = $1;
-			my $replace = $self->{'ansi_sequences'}->{'CSI'} . $s . 'S';
-			$text =~ s/\[\%\s+SCROLL UP $s\s+\%\]/$replace/gi;
-		}
-		while ($text =~ /\[\%\s+SCROLL DOWN (\d+)\s+\%\]/) {
-			my $s = $1;
-			my $replace = $self->{'ansi_sequences'}->{'CSI'} . $s . 'T';
-			$text =~ s/\[\%\s+SCROLL DOWN $s\s+\%\]/$replace/gi;
-		}
-		while ($text =~ /\[\%\s+RGB (\d+),(\d+),(\d+)\s+\%\]/) {
-			my ($r,$g,$b) = ($1 & 255, $2 & 255, $3 & 255);
-			my $replace = $self->{'ansi_sequences'}->{'CSI'} . "38:2:$r:$g:$b" . 'm';
-			$text =~ s/\[\%\s+RGB $r,$g,$b\s+\%\]/$replace/gi;
-		}
-		while ($text =~ /\[\%\s+B_RGB (\d+),(\d+),(\d+)\s+\%\]/) {
-			my ($r,$g,$b) = ($1 & 255, $2 & 255, $3 & 255);
-			my $replace = $self->{'ansi_sequences'}->{'CSI'} . "48:2:$r:$g:$b" . 'm';
-			$text =~ s/\[\%\s+B_RGB $r,$g,$b\s+\%\]/$replace/gi;
-		}
-		while ($text =~ /\[\%\s+(COLOR|COLOUR) (\d+)\s+\%\]/) {
-			my $n = $1;
-			my $c = $2 & 255;
-			my $replace = $self->{'ansi_sequences'}->{'CSI'} . "38:5:$c" . 'm';
-			$text =~ s/\[\%\s+$n $c\s+\%\]/$replace/gi;
-		}
-		while ($text =~ /\[\%\s+(B_COLOR|B_COLOUR) (\d+)\s+\%\]/) {
-			my $n = $1;
-			my $c = $2 & 255;
-			my $replace = $self->{'ansi_sequences'}->{'CSI'} . "48:5:$c" . 'm';
-			$text =~ s/\[\%\s+$n $c\s+\%\]/$replace/gi;
-		}
-		while ($text =~ /\[\%\s+GREY (\d+)\s+\%\]/) {
-			my $g = $1;
-			my $replace = $self->{'ansi_sequences'}->{'CSI'} . '38:5:' . (232 + $g) . 'm';
-			$text =~ s/\[\%\s+GREY $g\s+\%\]/$replace/gi;
-		}
-		while ($text =~ /\[\%\s+B_GREY (\d+)\s+\%\]/) {
-			my $g = $1;
-			my $replace = $self->{'ansi_sequences'}->{'CSI'} . '48:5:' . (232 + $g) . 'm';
-			$text =~ s/\[\%\s+B_GREY $g\s+\%\]/$replace/gi;
-		}
-		while ($text =~ /\[\%\s+BOX (.*?),(\d+),(\d+),(\d+),(\d+),(.*?)\s+\%\](.*?)\[\%\s+ENDBOX\s+\%\]/i) {
-			my $replace = $self->box($1, $2, $3, $4, $5, $6, $7);
-			$text =~ s/\[\%\s+BOX.*?\%\].*?\[\%\s+ENDBOX.*?\%\]/$replace/i;
-		}
+    if (length($text) > 1) {
+        while ($text =~ /\[\%\s+LOCATE (\d+),(\d+)\s+\%\]/) {
+            my ($r,$c) = ($1,$2);
+            my $replace = $self->{'ansi_sequences'}->{'CSI'} . "$r;$c" . 'H';
+            $text =~ s/\[\%\s+LOCATE $r,$c\s+\%\]/$replace/g;
+        }
+        while ($text =~ /\[\%\s+SCROLL UP (\d+)\s+\%\]/) {
+            my $s = $1;
+            my $replace = $self->{'ansi_sequences'}->{'CSI'} . $s . 'S';
+            $text =~ s/\[\%\s+SCROLL UP $s\s+\%\]/$replace/gi;
+        }
+        while ($text =~ /\[\%\s+SCROLL DOWN (\d+)\s+\%\]/) {
+            my $s = $1;
+            my $replace = $self->{'ansi_sequences'}->{'CSI'} . $s . 'T';
+            $text =~ s/\[\%\s+SCROLL DOWN $s\s+\%\]/$replace/gi;
+        }
+        while ($text =~ /\[\%\s+RGB (\d+),(\d+),(\d+)\s+\%\]/) {
+            my ($r,$g,$b) = ($1 & 255, $2 & 255, $3 & 255);
+            my $replace = $self->{'ansi_sequences'}->{'CSI'} . "38:2:$r:$g:$b" . 'm';
+            $text =~ s/\[\%\s+RGB $r,$g,$b\s+\%\]/$replace/gi;
+        }
+        while ($text =~ /\[\%\s+B_RGB (\d+),(\d+),(\d+)\s+\%\]/) {
+            my ($r,$g,$b) = ($1 & 255, $2 & 255, $3 & 255);
+            my $replace = $self->{'ansi_sequences'}->{'CSI'} . "48:2:$r:$g:$b" . 'm';
+            $text =~ s/\[\%\s+B_RGB $r,$g,$b\s+\%\]/$replace/gi;
+        }
+        while ($text =~ /\[\%\s+(COLOR|COLOUR) (\d+)\s+\%\]/) {
+            my $n = $1;
+            my $c = $2 & 255;
+            my $replace = $self->{'ansi_sequences'}->{'CSI'} . "38:5:$c" . 'm';
+            $text =~ s/\[\%\s+$n $c\s+\%\]/$replace/gi;
+        }
+        while ($text =~ /\[\%\s+(B_COLOR|B_COLOUR) (\d+)\s+\%\]/) {
+            my $n = $1;
+            my $c = $2 & 255;
+            my $replace = $self->{'ansi_sequences'}->{'CSI'} . "48:5:$c" . 'm';
+            $text =~ s/\[\%\s+$n $c\s+\%\]/$replace/gi;
+        }
+        while ($text =~ /\[\%\s+GREY (\d+)\s+\%\]/) {
+            my $g = $1;
+            my $replace = $self->{'ansi_sequences'}->{'CSI'} . '38:5:' . (232 + $g) . 'm';
+            $text =~ s/\[\%\s+GREY $g\s+\%\]/$replace/gi;
+        }
+        while ($text =~ /\[\%\s+B_GREY (\d+)\s+\%\]/) {
+            my $g = $1;
+            my $replace = $self->{'ansi_sequences'}->{'CSI'} . '48:5:' . (232 + $g) . 'm';
+            $text =~ s/\[\%\s+B_GREY $g\s+\%\]/$replace/gi;
+        }
+        while ($text =~ /\[\%\s+BOX (.*?),(\d+),(\d+),(\d+),(\d+),(.*?)\s+\%\](.*?)\[\%\s+ENDBOX\s+\%\]/i) {
+            my $replace = $self->box($1, $2, $3, $4, $5, $6, $7);
+            $text =~ s/\[\%\s+BOX.*?\%\].*?\[\%\s+ENDBOX.*?\%\]/$replace/i;
+        }
 
-		while ($text =~ /\[\%\s+(.*?)\s+\%\]/ && (exists($self->{'ansi_sequences'}->{$1}) || defined(charnames::string_vianame($1)))) {
-			my $string = $1;
-			if (exists($self->{'ansi_sequences'}->{$string})) {
-				if ($string =~ /CLS/i && ($self->{'sysop'} || $self->{'local_mode'})) {
-					my $ch = locate(($self->{'CACHE'}->get('START_ROW') + $self->{'CACHE'}->get('ROW_ADJUST')), 1) . cldown;
-					$text =~ s/\[\%\s+$string\s+\%\]/$ch/gi;
-				} else {
-					$text =~ s/\[\%\s+$string\s+\%\]/$self->{'ansi_sequences'}->{$string}/gi;
-				}
-			} else {
-				my $char = charnames::string_vianame($string);
-				$char = '?' unless (defined($char));
-				$text =~ s/\[\%\s+$string\s+\%\]/$char/gi;
-			}
-		}
-	}
-	return($text);
+        while ($text =~ /\[\%\s+(.*?)\s+\%\]/ && (exists($self->{'ansi_sequences'}->{$1}) || defined(charnames::string_vianame($1)))) {
+            my $string = $1;
+            if (exists($self->{'ansi_sequences'}->{$string})) {
+                if ($string =~ /CLS/i && ($self->{'sysop'} || $self->{'local_mode'})) {
+                    my $ch = locate(($self->{'CACHE'}->get('START_ROW') + $self->{'CACHE'}->get('ROW_ADJUST')), 1) . cldown;
+                    $text =~ s/\[\%\s+$string\s+\%\]/$ch/gi;
+                } else {
+                    $text =~ s/\[\%\s+$string\s+\%\]/$self->{'ansi_sequences'}->{$string}/gi;
+                }
+            } else {
+                my $char = charnames::string_vianame($string);
+                $char = '?' unless (defined($char));
+                $text =~ s/\[\%\s+$string\s+\%\]/$char/gi;
+            }
+        }
+    }
+    return($text);
 }
 
 sub ansi_output {
     my $self = shift;
     my $text = shift;
 
-	$text = $self->ansi_decode($text);
-	$text =~ s/\[ \% TOKEN \% \]/\[\% TOKEN \%\]/;
-	print $text;
+    $text = $self->ansi_decode($text);
+    $text =~ s/\[ \% TOKEN \% \]/\[\% TOKEN \%\]/;
+    print $text;
     return (TRUE);
 } ## end sub ansi_output
 
@@ -203,8 +203,8 @@ sub new {
     my $self = {
         'ansi_prefix'    => $csi,
         'mode'           => 'long',
-		'start'          => 0x2010,
-		'finish'         => 0x1FBFF,
+        'start'          => 0x2010,
+        'finish'         => 0x1FBFF,
         'ansi_sequences' => {
             'SS2'      => $esc . 'N',
             'SS3'      => $esc . 'O',
