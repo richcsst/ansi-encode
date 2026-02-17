d1ce6c488eabf25224519a7df7155535d2bfc746
Richard Kelsch <rich@csst.net>
2026-02-17 14:58:20 -0700

    Uploaded version

--- lib/Term/ANSIEncode.pm
+++ lib/Term/ANSIEncode.pm
@@ -1,4 +1,4 @@
-package Term::ANSIEncode 1.89;
+package Term::ANSIEncode 1.90;
 
 #######################################################################
 #            _   _  _____ _____   ______                     _        #
@@ -59,7 +59,7 @@ BEGIN {
     our @EXPORT_OK = qw(ansi_colors);
 } ## end BEGIN
 
-our $VERSION = '1.89';
+our $VERSION = '1.90';
 
 # Package-level caches so large tables are built only once per process.
 our $GLOBAL_ANSI_META = _global_ansi_meta();
