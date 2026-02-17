593bfe414da00f0ffb16fa8c4825ab7ceb25ebaa
Richard Kelsch <rich@csst.net>
2026-02-17 16:05:22 -0700

    Updates to the perldoc

--- lib/Term/ANSIEncode.pm
+++ lib/Term/ANSIEncode.pm
@@ -2103,3 +2103,13 @@ Richard Kelsch
 This program is free software; you can redistribute it and/or modify it under the terms of the the Artistic License (2.0). You may obtain a copy of the full license at;
 
 L<http://www.perlfoundation.org/artistic_license_2_0>
+
+=head1 GITHUB
+
+=over 4
+
+=item https://github.com/richcsst/ansi-encode
+
+=back
+
+=cut
