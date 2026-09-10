cdd208950088a6a5988f5f5d540e474d57d98743
Richard Kelsch <richcsst@users.noreply.github.com>
2026-09-09 19:21:09 -0600

    Bump version from 2.07 to 2.08

--- lib/Term/ANSIEncode.pm
+++ lib/Term/ANSIEncode.pm
@@ -1,4 +1,4 @@
-package Term::ANSIEncode 2.07;
+package Term::ANSIEncode 2.08;
 
 #######################################################################
 #            _   _  _____ _____   ______                     _        #
@@ -59,7 +59,7 @@ BEGIN {
     our @EXPORT_OK = qw(ansi_colors);
 } ## end BEGIN
 
-our $VERSION = '2.07';
+our $VERSION = '2.08';
 
 # Package-level caches so large tables are built only once per process.
 our $GLOBAL_ANSI_META = _global_ansi_meta();
